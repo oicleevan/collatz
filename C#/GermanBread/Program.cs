f325d5e25997973f860b08471d3320a11bbe6deb
GermanBread <nd01@online.de>
2021-09-04 23:16:44 +0200

    new: C# implementation

---
+++ C#/GermanBread/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+static List<uint> Collatz(uint seed) {
+    List<uint> _history = new();
+    _history.Add(seed);
+
+    while (seed > 1)
+        if (seed % 2 == 0)
+            seed /= 2;
+        else
+            {
+                seed *= 3;
+                seed++;
+            }
+        _history.Add(seed);
+
+    return _history;
+}
+
+Console.Write("Starting number: ");
+if (!uint.TryParse(Console.ReadLine(), out uint Seed)) {
+    Console.Error.WriteLine("The input entered was invalid.");
+    Environment.Exit(1);
+}
+
+var History = Collatz(Seed);
+
+Console.WriteLine(Seed);
+
+Console.WriteLine("Done!");
+Console.WriteLine($"Took {History.Count} passes.");
+Console.WriteLine($"History: {string.Join(' ', History)}");
---
+++ C#/GermanBread/README.md
@@ -0,0 +1,9 @@
+# Collatz Conjecture
+
+## Implementation by [GermanBread](https://github.com/GermanBread)
+
+### Running
+
+- install .NET 5 SDK
+- run `dotnet run` (cli args are ignored, you will be prompted for a number in the app instead)
+- profit
