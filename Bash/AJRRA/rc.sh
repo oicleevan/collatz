90303bef68f1d4e5e26f224548ed5456c337ad8e
Evan Oicle <50642010+oicleevan@users.noreply.github.com>
2021-09-04 16:06:00 -0400

    Revert "Add Bash"

--- Bash/AJRRA/rc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ $# -eq 1 ]
-then
-    echo "The results are in [`bash collatz.sh $1 `]"
-else
-    read -p "Please enter a number: " temp
-    echo "The results are in [`bash collatz.sh $temp `]"
-fi
